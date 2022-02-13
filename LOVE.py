a74c60f184926aacb6b379badad7ee87a1c831ba
PROLOY <iamproloy9@gmail.com>
2022-02-13 21:46:56 +0600

    master

--- LOVE.py
+++ LOVE.py
@@ -1,5 +1,4 @@
-iDo I love you? My god, if your love were a grain of sand, mine would be a universe of beaches.
-mport random
+import random
 import pyautogui
 import time
 a=['I saw that you were perfect, and so I loved you. Then I saw that you were not perfect and I loved you even more.','You know you’re in love when you can’t fall asleep because reality is finally better than your dreams','Love is that condition in which the happiness of another person is essential to your own.','The best thing to hold onto in life is each other.','I need you like a heart needs a beat.','I am who I am because of you. You are every reason, every hope, and every dream I’ve ever had.','If I had a flower for every time I thought of you… I could walk through my garden forever.','Take my hand, take my whole life too. For I can’t help falling in love with you','If you live to be a hundred, I want to live to be a hundred minus one day so I never have to live without you.','You’re the closest to heaven, that I’ll ever be.','You are the finest, loveliest, tenderest, and most beautiful person I have ever known and even that is an understatement.','I will never stop trying. Because when you find the one… you never give up.','It’s always better when we’re together.','I love you for all that you are, all that you have been and all that you will be',
